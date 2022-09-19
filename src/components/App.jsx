1fe7323fc1511e2c7b21b96db8a871a317c4b35d
Anatolii <anatoly.tomnyak@gmail.com>
2022-09-20 01:55:49 +0300

    start making

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import './App.css'
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import { nanoid } from 'nanoid'
+
+
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  };
+
+
+  render() {
+    
+    return (
+      <div className="phonebook">
+        <Section title="Phonebook">
+          <form className="phonebook__form">
+            <label className="phonebook__label">
+              Name
+              <input
+                className='phonebook__input'
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              />
+              <button type='submit' className='phonebook__btn-add'>Add</button>
+            </label>
+            </form>
+        </Section>
+        <Section title="Contacts">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </Section>
+        
+      </div>
+    );
+  }
+}
+  
+export default App;
